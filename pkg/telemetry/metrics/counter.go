4a4574b133e285b7fcaf2f967f0bc3287fc9b185
Riadh <22998716+KiKoS0@users.noreply.github.com>
2025-11-10 21:22:05 +0100

    Track pause deletions at the block level
    - Make `blockIDsForTimestamp` returns the first block that's >= pause
      timestamp, along with the next one if the pause timestamp is equal to
      the block boundary (it means that the pause may be in both)
    - Handle legacy pauses with no timestamp by marking them as deleted on
      all blocks and reduce the compaction trigger frequency in that case.
    - Add tests for the boundary case and zero timestamp cases.

--- pkg/telemetry/metrics/counter.go
+++ pkg/telemetry/metrics/counter.go
@@ -680,3 +680,12 @@ func IncrQueueDatabaseContextTimeoutCounter(ctx context.Context, opts CounterOpt
 		Tags:        opts.Tags,
 	})
 }
+
+func IncrPausesLegacyDeletionCounter(ctx context.Context, opts CounterOpt) {
+	RecordCounterMetric(ctx, 1, CounterOpt{
+		PkgName:     opts.PkgName,
+		MetricName:  "pauses_legacy_deletion_total",
+		Description: "Total number of legacy pause deletions without timestamps",
+		Tags:        opts.Tags,
+	})
+}
